a135f9b5a8ac0df35fafb6ce325735337644b9e3
luisgdj <luisgdj02@gmail.com>
2023-05-04 18:14:19 +0200

    jpa

---
+++ src/bloodbank/pojos/User.java
@@ -0,0 +1,100 @@
+package bloodbank.pojos;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+private static final long serialVersionUID = 1852408272326099491L;
+	
+	@Id
+	@GeneratedValue(generator = "users")
+	@TableGenerator(name = "users", table = "sqlite_sequence",
+		pkColumnName = "name", valueColumnName = "seq", pkColumnValue = "users")
+	private Integer id;
+	@Column(unique = true)
+	private String username;
+	private String password;
+	private String email;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "roleId")
+	private Role role;
+	
+	public User() {
+		super();
+	}
+
+	public User(String username, String password, String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + ", role="
+				+ role + "]";
+	}
+}

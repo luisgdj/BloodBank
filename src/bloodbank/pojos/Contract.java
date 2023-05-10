b528129b18500616716ca542f2f205f9fad347c2
Angelo-De-Nadai <80247207+Angelo-De-Nadai@users.noreply.github.com>
2023-05-10 19:07:14 +0200

    XML of last Object(Contract)

--- src/bloodbank/pojos/Contract.java
+++ src/bloodbank/pojos/Contract.java
@@ -5,6 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Contract")
+@XmlType(propOrder = { "duration", "salary", "nurses" })
 public class Contract implements Serializable {
 
 	/**
@@ -12,32 +23,38 @@ public class Contract implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@XmlTransient
 	private Integer id;
+	@XmlElement
 	private Integer duration;
+	@XmlElement
 	private float salary;
+	@XmlElement(name = "Nurse")
+    @XmlElementWrapper(name = "Nurses")	
 	private List<Nurse> nurses;
 
 	public Contract() {
-		//establecemos un contrato por defecto que tendran todas las nurses inicialmente
+		// establecemos un contrato por defecto que tendran todas las nurses
+		// inicialmente
 		super();
 		this.id = 0;
 		this.duration = 12;
 		this.salary = 2500;
 	}
-	
+
 	public Contract(int duration, float salary) {
 		super();
 		this.duration = duration;
 		this.salary = salary;
 		this.nurses = new ArrayList<Nurse>();
-	} 
+	}
 
 	public Contract(int id, int duration, float salary) {
 		super();
 		this.id = id;
 		this.duration = duration;
 		this.salary = salary;
-	} 
+	}
 
 	public int getId() {
 		return id;
@@ -70,13 +87,12 @@ public class Contract implements Serializable {
 	public void setNurse(Nurse nurse) {
 		this.nurses = (List<Nurse>) nurses;
 	}
-	
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)

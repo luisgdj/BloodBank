7eb517a382a8d8a09da2801aac8ca9a21c2e8134
Angelo-De-Nadai <80247207+Angelo-De-Nadai@users.noreply.github.com>
2023-05-11 16:54:24 +0200

    Add files via upload

---
+++ src/LocalDateAdapter.java
@@ -0,0 +1,20 @@
+package xml.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    @Override
+    public LocalDate unmarshal(String dateString) throws Exception {
+        return LocalDate.parse(dateString, DATE_FORMAT);
+    }
+
+    @Override
+    public String marshal(LocalDate date) throws Exception {
+        return date.format(DATE_FORMAT);
+    }
+}

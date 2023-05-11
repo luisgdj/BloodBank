da458f126392e281187cd136f830f46f0e344a75
luisgdj <luisgdj02@gmail.com>
2023-05-11 16:57:43 +0200

    .

--- src/bloodbank/pojos/Blood.java
+++ src/bloodbank/pojos/Blood.java
@@ -13,7 +13,7 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import xml.utils.LocalDateAdapter;
+import bloodbank.xml.utils.LocalDateAdapter;
 
 
 
--- src/bloodbank/pojos/Donor.java
+++ src/bloodbank/pojos/Donor.java
@@ -14,7 +14,7 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import xml.utils.LocalDateAdapter;
+import bloodbank.xml.utils.LocalDateAdapter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "Donor")
